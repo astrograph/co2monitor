eea51b1a60d91e6e28728b5f76aa22e2abd424a6
Philipp Salgeber <philipp@salzgeber.at>
2019-04-15 23:10:55 +0100

    use absolute path for upload

--- upload.sh
+++ upload.sh
@@ -1,6 +1,5 @@
 #/bin/bash
-latest_csv_file=$(ls -dt *.csv | head -1)
-echo $latest_csv_file
-curl -T /home/pi/co2monitor/$latest_csv_file -u $FTP_USER:$FTP_PASSWORD $FTP_HOST/co2.csv
-curl -T /home/pi/co2monitor/$latest_csv_file -u $FTP_USER:$FTP_PASSWORD $FTP_HOST/
+latest_csv_file=$(ls -dt /home/pi/co2monitor/*.csv | head -1)
+curl -T $latest_csv_file -u $FTP_USER:$FTP_PASSWORD $FTP_HOST/co2.csv
+curl -T $latest_csv_file -u $FTP_USER:$FTP_PASSWORD $FTP_HOST/
 
